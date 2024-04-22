50399b5ba9802fae58ec446cee6ffc2aab532a75
Hena Naaz <henanaazkhan24@gmail.com>
2024-04-22 02:12:27 -0400

    Update runall.sh

--- SCRIPTS/runall.sh
+++ SCRIPTS/runall.sh
@@ -3,10 +3,10 @@
 ## Run one benchmark
 #####################################################################################################
 
-#mkdir -p ../RESULTS/BASELINE.4C.8MB; ./sim ../TRACES/bwaves_06.mat.gz ../TRACES/bwaves_06.mat.gz ../TRACES/bwaves_06.mat.gz ../TRACES/bwaves_06.mat.gz > ../RESULTS/BASELINE.4C.8MB/src_rrs_exp_1k_bwaves_06.res ; 
-#mkdir -p ../RESULTS/BASELINE.4C.8MB; ./sim ../TRACES/bzip2_06.mat.gz ../TRACES/bzip2_06.mat.gz ../TRACES/bzip2_06.mat.gz ../TRACES/bzip2_06.mat.gz > ../RESULTS/BASELINE.4C.8MB/src_rrs_exp_1k_bzip2_06.res ; 
-#mkdir -p ../RESULTS/BASELINE.4C.8MB; ./sim ../TRACES/cactusADM_06.mat.gz ../TRACES/cactusADM_06.mat.gz ../TRACES/cactusADM_06.mat.gz ../TRACES/cactusADM_06.mat.gz > ../RESULTS/BASELINE.4C.8MB/src_rrs_exp_1k_cactusADM_06.res ; 
-#mkdir -p ../RESULTS/BASELINE.4C.8MB; ./sim ../TRACES/GemsFDTD_06.mat.gz ../TRACES/GemsFDTD_06.mat.gz ../TRACES/GemsFDTD_06.mat.gz ../TRACES/GemsFDTD_06.mat.gz > ../RESULTS/BASELINE.4C.8MB/src_rrs_exp_1k_GemsFDTD_06.res ; 
+mkdir -p ../RESULTS/BASELINE.4C.8MB; ./sim ../TRACES/bwaves_06.mat.gz ../TRACES/bwaves_06.mat.gz ../TRACES/bwaves_06.mat.gz ../TRACES/bwaves_06.mat.gz > ../RESULTS/BASELINE.4C.8MB/src_rrs_exp_1k_bwaves_06.res ; 
+mkdir -p ../RESULTS/BASELINE.4C.8MB; ./sim ../TRACES/bzip2_06.mat.gz ../TRACES/bzip2_06.mat.gz ../TRACES/bzip2_06.mat.gz ../TRACES/bzip2_06.mat.gz > ../RESULTS/BASELINE.4C.8MB/src_rrs_exp_1k_bzip2_06.res ; 
+mkdir -p ../RESULTS/BASELINE.4C.8MB; ./sim ../TRACES/cactusADM_06.mat.gz ../TRACES/cactusADM_06.mat.gz ../TRACES/cactusADM_06.mat.gz ../TRACES/cactusADM_06.mat.gz > ../RESULTS/BASELINE.4C.8MB/src_rrs_exp_1k_cactusADM_06.res ; 
+mkdir -p ../RESULTS/BASELINE.4C.8MB; ./sim ../TRACES/GemsFDTD_06.mat.gz ../TRACES/GemsFDTD_06.mat.gz ../TRACES/GemsFDTD_06.mat.gz ../TRACES/GemsFDTD_06.mat.gz > ../RESULTS/BASELINE.4C.8MB/src_rrs_exp_1k_GemsFDTD_06.res ; 
 mkdir -p ../RESULTS/BASELINE.4C.8MB; ./sim ../TRACES/mcf_06.mat.gz ../TRACES/mcf_06.mat.gz ../TRACES/mcf_06.mat.gz ../TRACES/mcf_06.mat.gz > ../RESULTS/BASELINE.4C.8MB/src_rrs_exp_1k_mcf_06.res ; 
 mkdir -p ../RESULTS/BASELINE.4C.8MB; ./sim ../TRACES/zeusmp_06.mat.gz ../TRACES/zeusmp_06.mat.gz ../TRACES/zeusmp_06.mat.gz ../TRACES/zeusmp_06.mat.gz > ../RESULTS/BASELINE.4C.8MB/src_rrs_exp_1k_zeusmp_06.res ; 
 
